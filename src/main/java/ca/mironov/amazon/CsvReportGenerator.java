bd78a4819f3d7c810bd9b41cebb1f2269498d3b6
Stanislav Mironov <smironov@gmail.com>
2018-11-05 18:28:45 -0500

    Added totals to reports

--- src/main/java/ca/mironov/amazon/CsvReportGenerator.java
+++ src/main/java/ca/mironov/amazon/CsvReportGenerator.java
@@ -3,6 +3,7 @@ package ca.mironov.amazon;
 import org.apache.commons.csv.*;
 
 import java.io.*;
+import java.math.BigDecimal;
 import java.nio.file.*;
 import java.util.List;
 
@@ -24,11 +25,29 @@ public class CsvReportGenerator implements ReportGenerator {
             try (CSVPrinter csvPrinter = new CSVPrinter(Files.newBufferedWriter(reportFile), CSVFormat.EXCEL)) {
                 // print headers
                 csvPrinter.printRecord("Order #", "Items subtotal", "Shipping & handling", "Environmental handling fee", "Total before tax", "HST", "Total", "Items");
+                BigDecimal totalItemsSubtotal = BigDecimal.ZERO;
+                BigDecimal totalShippingAndHandling = BigDecimal.ZERO;
+                BigDecimal totalEnvironmentalHandlingFee = BigDecimal.ZERO;
+                BigDecimal totalTotalBeforeTax = BigDecimal.ZERO;
+                BigDecimal totalHst = BigDecimal.ZERO;
+                BigDecimal totalTotal = BigDecimal.ZERO;
                 for (Order order : orders) {
                     csvPrinter.printRecord(order.getId(),
                             order.getItemsSubtotal(), order.getShippingAndHandling(), order.getEnvironmentalHandlingFee(),
                             order.getTotalBeforeTax(), order.getHst(), order.getTotal(), order.getItems());
+                    totalItemsSubtotal = totalItemsSubtotal.add(order.getItemsSubtotal());
+                    totalShippingAndHandling = totalShippingAndHandling.add(order.getShippingAndHandling());
+                    totalEnvironmentalHandlingFee = totalEnvironmentalHandlingFee.add(order.getEnvironmentalHandlingFee());
+                    totalTotalBeforeTax = totalTotalBeforeTax.add(order.getTotalBeforeTax());
+                    totalHst = totalHst.add(order.getHst());
+                    totalTotal = totalTotal.add(order.getTotal());
                 }
+                // print total
+                csvPrinter.printRecord();
+                csvPrinter.printRecord(
+                        "Total:", totalItemsSubtotal, totalShippingAndHandling, totalEnvironmentalHandlingFee,
+                        totalTotalBeforeTax, totalHst, totalTotal
+                );
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
